41fd2e6eb913ee3cc0e201ac858c07a4a86a92d2
ccamposcr <christian.campos@digitaslbi.com>
2018-07-23 12:35:55 -0600

    SR_SHAW-EX-V2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 	res.render('home', {title:'SR SHAW Exercise'});
 });
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 //app.use('/', routes);
 
 app.listen(3000, function(){
