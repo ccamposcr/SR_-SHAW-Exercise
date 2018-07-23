2efbfe01d2cfb55b6ed1814915cc548c17af0868
ccamposcr <christian.campos@digitaslbi.com>
2018-07-23 11:53:38 -0600

    SR_SHAW-EX-V2

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+//const routes = require('./routes/index');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+//view engine setup
+//app.set('views', path.join(__dirname, '/views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+	res.render('home', {title:'SR SHAW Exercise Handlebars'});
+});
+
+//app.use(express.static(__dirname + '/public'));
+//app.use('/', routes);
 
 app.listen(3000, function(){
 	console.log('Server on port 3000');
