c7d7d52ba6e3b0b25110dd55a4c68660fab684c4
ccamposcr <christian.campos@digitaslbi.com>
2018-07-24 23:56:58 -0600

    SR_SHAW-EX-V2

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/index.jsx',
   output:{
@@ -10,7 +12,22 @@ module.exports = {
         test: /\.jsx$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      }
+      }/*,
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader','sass-loader'],
+          publicPath: '/public/css'
+        })
+      }*/
     ]
-  }
+  }/*,
+  plugins:[
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      disabled: false,
+      allChunks: true
+    })
+  ]*/
 }
