eb70ec190bd0c7a1ebc435d1c648aad304e8490f
ccamposcr <christian.campos@digitaslbi.com>
2018-08-23 12:02:45 -0600

    refactoring code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,38 +10,19 @@ class Form extends Component{
       message: ''
     };
     this.handleSubmit.bind(this);
-    this.handleNameChange.bind(this);
-    this.handleEmailChange.bind(this);
-    this.handleMessageChange.bind(this);
+    this.handleChange.bind(this);
   }
 
-  handleNameChange(event){
+  handleChange(event){
+    const {name, value} = event.target;
     this.setState({
-      name: event.target.value
-    });
-  }
-
-  handleEmailChange(event){
-    this.setState({
-      email: event.target.value
+      [name]: value
     });
   }
 
   handleSubmit(event){
-    console.log('Form was submitted' + this.state.name);
+    console.log('Form was submitted');
     event.preventDefault();
-  }
-
-  handleSubjectChange(event){
-    this.setState({
-      subject: event.target.value
-    });
-  }
-
-  handleMessageChange(event){
-    this.setState({
-      message: event.target.value
-    });
   }
 
   render(){
@@ -49,10 +30,10 @@ class Form extends Component{
       <div class="form--wrapper">
         <h2 class="form--title">{this.props.form.formTitle}</h2>
         <form onSubmit={this.handleSubmit} class="form">
-          <label for="name">Name:*</label><input id="name" name="name" type="text" class="form--name" value={this.state.name} onChange={this.handleNameChange}/>
-          <label for="email">Email Address:*</label><input id="email" name="email" type="text" class="form--email" value={this.state.email} onChange={this.handleEmailChange}/>
-          <label for="subject">Subject:*</label><input id="subject" name="subject" type="text" class="form--subject" value={this.state.subject} onChange={this.handleSubjectChange}/>
-          <label for="message">Message:*</label><textarea id="message" name="message" value={this.state.message} class="form--message" onChange={this.handleMessageChange}></textarea>
+          <label for="name">Name:*</label><input id="name" name="name" type="text" class="form--name" value={this.state.name} onChange={this.handleChange}/>
+          <label for="email">Email Address:*</label><input id="email" name="email" type="text" class="form--email" value={this.state.email} onChange={this.handleChange}/>
+          <label for="subject">Subject:*</label><input id="subject" name="subject" type="text" class="form--subject" value={this.state.subject} onChange={this.handleChange}/>
+          <label for="message">Message:*</label><textarea id="message" name="message" value={this.state.message} class="form--message" onChange={this.handleChange}></textarea>
           <input class="form--submit" type="submit" value="Submit"/>
         </form>
       </div>
